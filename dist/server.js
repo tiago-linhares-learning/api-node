378d57c82bb10af593163519d3ef345e13caba82
Tiago <tiago.linhares@live.com>
2025-02-26 12:39:03 -0300

    commit

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = 8080;
 app.get("/", (req, res) => {
     res.json({ message: "API funcionando!" });
 });
